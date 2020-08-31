cc0e1813d9900b6f53c3bdcc17ebd155bd121cb8
Joshua Ramos <bassramos@gmail.com>
2020-08-31 18:27:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello Everybody!";
+var greeting = 'Hello Everybody!'
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = 'Neil deGrasse Tyson'
 
 var greetSpecialGuest = "Hello Neil deGrasse Tyson" + specialGuest + "!";
 
-var topic = "space";
+var topic = 'Let\'s talk about space';
 
 var conversation = `${topic}`;
